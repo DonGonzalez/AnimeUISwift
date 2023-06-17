22a352d7f67303afa266579df866ad520be2ee07
DonGonzalez <dongonzales818@gmail.com>
2023-06-17 12:45:53 +0200

    Initial Commit

---
+++ AnimeSwiftUI/AnimeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeSwiftUIApp.swift
+//  AnimeSwiftUI
+//
+//  Created by Robert B on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
